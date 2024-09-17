36de4b29cd9a08db780bf1e31cde719b56be0397
Marcin Mucha <marcin.mucha@bjss.com>
2024-09-17 14:38:39 +0100

    Chapter 5-Props
    add props to Home.jsx component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
-const Home = () => {
-  return <h1>Home</h1>;
+
+const Home = (props) => {
+    //Accessing the props object
+    // we can access the "className" prop to customize the class of the h1 element
+    const {className} = props;
+  return <h1 className={className}>Home</h1>;
+};
+//Default props object that contains certain predefined properties and methods.
+Home.defaultProps = {
+    className: 'home-title', // seting default className for h1 element
 };
 
 export default Home;
