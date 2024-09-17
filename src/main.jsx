a84d8fe3765ef9aaeb2f510e3cba6ac5863da3ff
Marcin Mucha <marcin.mucha@bjss.com>
2024-09-17 14:15:12 +0100

    Chapter 4-Routing

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,8 @@ import Car from './Car.jsx'
 import Counter from './Counter.jsx'
 
 
-import './index.css'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Car/>
-    <Counter/>
   </StrictMode>,
 )
---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,5 @@
+const Blogs = () => {
+  return <h1>Blog articles</h1>;
+};
+
+export default Blogs;
